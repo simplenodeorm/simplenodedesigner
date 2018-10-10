397edebe971764488cd7f767476a3e7be9c1ef2b
Rob Tucker <tucker87@msu.edu>
2018-10-09 23:11:01 -0400

    get model selection and tree logic working

--- src/components/FormatSelectionLine.js
+++ src/components/FormatSelectionLine.js
@@ -2,22 +2,56 @@ import React from 'react';
 import "../app/App.css";
 import config from '../config/appconfig.json';
 
+const dateAggFunction = <select><option></option><option>count</option><option>max</option><option>min</option></select>
+const numAggFunction = <select><option></option><option>avg</option><option>count</option><option>max</option><option>min</option><option>sum</option></select>
+const strAggFunction = <select><option></option><option>count</option></select>
+
 class FormatSelectionLine extends React.Component {
     constructor(props) {
         super(props);
+        
+        let fsel;
+        switch(this.getType(props.columnNode.type)) {
+            case "string":
+                fsel = strAggFunction;
+                break;
+            case "date":
+                fsel = dateAggFunction;
+                break;
+            case "number":
+                fsel = numAggFunction;
+                break;
+        }
+        
         this.state = {
             loading: false,
-            error: ''
+            error: '',
+            functionSelect: fsel
         };
     }
 
     render() {
         return <div className="formatSelectionLine">
-        <span className="label">{this.props.columnNode.__index + 1}.&nbsp;</span><span className="lineStyle1"><span>{this.props.columnNode.path}</span></span><br /><br />
+            <span className="label">{this.props.columnNode.__index + 1}.&nbsp;</span>
+            <span className="lineStyle1">{this.props.columnNode.path}</span>
+            <br />
+            <input type="text" value=''/>{this.state.functionSelect}
         </div>;
-        
     }
     
+    getType(dbType) {
+        let retval = 'string'
+        switch(dbType) {
+            case 'DATE':
+                retval = 'date';
+                break;
+            case 'NUMBER':
+                retval = 'number';
+                break;
+        }
+        
+        return retval;
+    }
 }
 
 export {FormatSelectionLine};
