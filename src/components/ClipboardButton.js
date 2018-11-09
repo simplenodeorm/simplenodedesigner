460be71a832860dd0cea3ec81acf9b095de3d709
Rob Tucker <tucker87@msu.edu>
2018-11-09 08:48:52 -0500

    add clipboard functionality

--- src/components/ClipboardButton.js
+++ src/components/ClipboardButton.js
@@ -3,18 +3,32 @@ import "../app/App.css";
 
 const clipboardDisabled = <img alt='copy to clipboard' src='/images/clipboard-disabled.png' />;              
 const clipboard = <img alt='copy to clipboard' src='/images/clipboard.png' />;               
+const clipboardData = <img alt='copy to clipboard' src='/images/clipboard-withdata.png' />;               
 
 class ClipboardButton extends React.Component {
     constructor(props) {
         super(props);
+        this.onCopyToClipboard = this.onCopyToClipboard.bind(this);
+        this.state = {
+            hasdata: false
+        };
     }
     
     render() {
+        const {hasdata} = this.state;
+        
         if (this.props.disabled) {
             return <button title="copy to clipboard" className="clipboardButton" disabled>{clipboardDisabled}</button>;              
+        } else if (hasdata) {
+            return <button title="copy to clipboard" className="clipboardButton" onClick={this.onCopyToClipboard}>{clipboardData}</button>;              
         } else {
-            return <button title="copy to clipboard" className="clipboardButton" onClick={this.props.onCopyToClipboard}>{clipboard}</button>;              
+            return <button title="copy to clipboard" className="clipboardButton" onClick={this.onCopyToClipboard}>{clipboard}</button>;              
         }
+    }
+    
+    onCopyToClipboard() {
+        this.props.onCopyToClipboard();
+        this.setState({hasdata:true})
     }
 }
 
