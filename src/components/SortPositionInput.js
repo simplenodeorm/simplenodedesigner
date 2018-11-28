818a5ada511115d5608da02434db2f8588975151
Rob Tucker <tucker87@msu.edu>
2018-11-28 10:20:09 -0500

    fix pre-populate ui issues when loading existing doc

--- src/components/SortPositionInput.js
+++ src/components/SortPositionInput.js
@@ -7,16 +7,22 @@ class SortPositionInput extends React.Component {
     constructor(props) {
         super(props);
         this.onBlur = this.onBlur.bind(this);
+        if (document.designData.currentDocument 
+            && document.designData.currentDocument.document.selectedColumns[this.props.index].sortPosition
+            && !document.designData.selnodes[this.props.index].__sortPosition) {
+            document.designData.selnodes[this.props.index].__sortPosition = document.designData.currentDocument.document.selectedColumns[this.props.index].sortPosition;
+        }
     }
     
     render() {
         return <span className="fieldLabel">{config.textmsg.sortposlabel}
-            <NumericInput maxLength='2' onBlur={this.onBlur} value={document.designData.selnodes[this.props.index].__sortPosition}/>
+            <NumericInput maxLength='2' onBlur={this.onBlur} defaultValue={document.designData.selnodes[this.props.index].__sortPosition}/>
             </span>;
     }
 
     onBlur(e) {
         document.designData.selnodes[this.props.index].__sortPosition = e.target.value;
+        document.designData.currentDocument.document.selectedColumns[this.props.index].sortPosition  = e.target.value;   
     }
 }
 
